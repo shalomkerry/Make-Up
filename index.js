81c677ab770de9b8248f9c827b8ac7c100273c74
shalomkerry <shalombadeg8@gmail.com>
2024-06-13 15:08:19 +0300

    always start with mobile first

--- index.js
+++ index.js
@@ -15,21 +15,4 @@ $("document").ready(function () {
     setTimeout(slides, 3000);
   }
   slides();
-  function scroll(n) {
-    clearTimeout(timeout);
-
-    setIndex += n;
-    if (setIndex > elements.length) {
-      setIndex = 1;
-    }
-    if (setIndex < 1) {
-      setIndex = setIndex.length;
-    }
-    elements.each(function () {
-      $(this).removeClass("active");
-    });
-    elements.eq(setIndex - 1).addClass("active");
-    console.log("yes");
-    timeout = setTimeout(slides, 3000);
-  }
 });
